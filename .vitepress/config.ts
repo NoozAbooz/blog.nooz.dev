25e6f200834d900e1ac2c8c2b20f29fb04b3b87b
Michael Zheng <zhengm58@gmail.com>
2026-03-21 05:00:19 -0600

    oh brother

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,9 @@ import sidebar from './sidebar.ts'
 import { withMermaid } from "vitepress-plugin-mermaid";
 
 export default withMermaid({
+  head: [
+	['link', { rel: 'icon', href: '/favicon.ico' }],
+  ],
   vite: {
     optimizeDeps: { include: ['@braintree/sanitize-url'] },
     resolve: {
